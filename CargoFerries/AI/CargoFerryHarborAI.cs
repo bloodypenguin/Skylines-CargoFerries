a00e31393504a8679cd76d5b70551f191492a890
Bloody Penguin <bloodypenguin.skylines@gmail.com>
2021-04-24 21:10:28 +0200

    Inherit station AI from CargoStationAI and not CargoHarborAI

--- CargoFerries/AI/CargoFerryHarborAI.cs
+++ CargoFerries/AI/CargoFerryHarborAI.cs
@@ -1,11 +1,39 @@
+using System;
+using ColossalFramework;
 using ColossalFramework.Math;
 using UnityEngine;
 
 namespace CargoFerries.AI
 {
-    public class CargoFerryHarborAI : CargoHarborAI
+    
+    //based of CargoHarborAI but without animals, connections & checking height
+    public class CargoFerryHarborAI : CargoStationAI
     {
-        //disables checking for CannotConnect error
+        [NonSerialized] protected float m_quayOffset;
+
+        public override void InitializePrefab()
+        {
+            base.InitializePrefab();
+            float a = this.m_info.m_generatedInfo.m_max.z - 7f;
+            if (this.m_info.m_paths != null)
+            {
+                for (int index1 = 0; index1 < this.m_info.m_paths.Length; ++index1)
+                {
+                    if (this.m_info.m_paths[index1].m_netInfo != null &&
+                        this.m_info.m_paths[index1].m_netInfo.m_class.m_service == ItemClass.Service.Road &&
+                        this.m_info.m_paths[index1].m_nodes != null)
+                    {
+                        for (int index2 = 0; index2 < this.m_info.m_paths[index1].m_nodes.Length; ++index2)
+                            a = Mathf.Min(a,
+                                -16f - this.m_info.m_paths[index1].m_netInfo.m_halfWidth -
+                                this.m_info.m_paths[index1].m_nodes[index2].z);
+                    }
+                }
+            }
+
+            this.m_quayOffset = a;
+        }
+
         public override ToolBase.ToolErrors CheckBuildPosition(
             ushort relocateID,
             ref Vector3 position,
@@ -16,9 +44,36 @@ namespace CargoFerries.AI
             out int productionRate,
             out int constructionCost)
         {
-            var errors = base.CheckBuildPosition(relocateID, ref position, ref angle, waterHeight, elevation,
-                ref connectionSegment, out productionRate, out constructionCost);
-            return errors & ~ToolBase.ToolErrors.CannotConnect;
+            ToolBase.ToolErrors toolErrors1 = ToolBase.ToolErrors.None;
+            Vector3 pos;
+            Vector3 dir;
+            bool isQuay;
+            if (this.m_info.m_placementMode == BuildingInfo.PlacementMode.Shoreline &&
+                BuildingTool.SnapToCanal(position, out pos, out dir, out isQuay, 40f, false))
+            {
+                angle = Mathf.Atan2(dir.x, -dir.z);
+                pos += dir * this.m_quayOffset;
+                position.x = pos.x;
+                position.z = pos.z;
+                if (!isQuay)
+                    toolErrors1 |= ToolBase.ToolErrors.ShoreNotFound;
+            }
+
+            ToolBase.ToolErrors toolErrors2 = toolErrors1 | base.CheckBuildPosition(relocateID, ref position, ref angle,
+                waterHeight, elevation, ref connectionSegment, out productionRate, out constructionCost);
+
+            return toolErrors2;
         }
+
+        public override bool GetWaterStructureCollisionRange(out float min, out float max)
+        {
+            if (this.m_info.m_placementMode != BuildingInfo.PlacementMode.Shoreline)
+                return base.GetWaterStructureCollisionRange(out min, out max);
+            min = 20f / Mathf.Max(22f, (float) this.m_info.m_cellLength * 8f);
+            max = 1f;
+            return true;
+        }
+
+        public override bool RequireRoadAccess() => true;
     }
 }
