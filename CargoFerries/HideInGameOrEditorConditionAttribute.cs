50f1e5619da6b2b91fdc092117e766fd906ff6b9
Bloody Penguin <bloodypenguin.skylines@gmail.com>
2021-05-03 20:41:18 +0200

    Hide the warehouse option outside of the main menu

---
+++ CargoFerries/HideInGameOrEditorConditionAttribute.cs
@@ -0,0 +1,15 @@
+using System;
+using CargoFerries.OptionsFramework.Attibutes;
+
+namespace CargoFerries
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class HideInGameOrEditorConditionAttribute : HideConditionAttribute
+    {
+        public override bool IsHidden()
+        {
+            return SimulationManager.exists && SimulationManager.instance.m_metaData != null &&
+                   SimulationManager.instance.m_metaData.m_updateMode != SimulationManager.UpdateMode.Undefined;
+        }
+    }
+}
--- CargoFerries/Options.cs
+++ CargoFerries/Options.cs
@@ -6,6 +6,7 @@ namespace CargoFerries
     [Options("Barges-Options")]
     public class Options
     {
+        [HideInGameOrEditorCondition]
         [Checkbox("Built-in warehouse for barge harbors (Industries DLC is required)")]
         public bool EnableWarehouseAI { get; set; } = true;
     }
